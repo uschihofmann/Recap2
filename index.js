8c2f3391af0fcbbbc45cdb4bf4491f2dd122ef84
Uschi <uschihofmann@gmx.de>
2024-12-12 11:28:20 +0100

    implemented toggle function for answer button

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// phew… not a lot going on here. Please add some code!
+/* // phew… not a lot going on here. Please add some code!
+//toggle bookmarkbutton*/
+const bookmarkbutton = document.querySelector('[data-js="bookmark-button"]');
+console.log(bookmarkbutton);
+
+bookmarkbutton.addEventListener("click", () => {
+  console.log("action");
+  bookmarkbutton.classList.toggle("bookmark--active");
+});
+
+//TOGGLE ANSWER BUTTON
+
+const answerbutton = document.querySelector('[data-js="answer-button"]');
+const answercard = document.querySelector('[data-js="card-answer"]');
+
+answerbutton.addEventListener("click", () => {
+  answercard.classList.toggle("card__answer--active");
+  if (answercard.classList.contains("card__answer--active")) {
+    answerbutton.textContent = "Hide answer";
+  } else {
+    answerbutton.textContent = "Show answer";
+  }
+});
